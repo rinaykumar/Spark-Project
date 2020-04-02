165666a43b6317dbdd57abe00bcbc5ad11c16361
Girish Rawat <emailgirishrawat@gmail.com>
2020-04-02 16:00:08 -0700

    Added DAO and DTO casses

---
+++ src/main/java/DTO/TransactionDTO.java
@@ -0,0 +1,12 @@
+
+package DTO;
+
+public class TransactionDTO {
+    public final String paymentMethod;
+    public final String itemCode;
+
+    public TransactionDTO(String paymentMethod, String itemCode) {
+        this.paymentMethod = paymentMethod;
+        this.itemCode = itemCode;
+    }
+}
