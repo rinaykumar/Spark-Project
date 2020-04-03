7849b38738c10ab4957934a1e0d49e3f062b1659
Girish Rawat <emailgirishrawat@gmail.com>
2020-04-02 22:48:08 -0700

    debugging transactionDAO

--- src/main/java/DAO/TransactionDAO.java
+++ src/main/java/DAO/TransactionDAO.java
@@ -1,9 +1,14 @@
 package DAO;
 
 import DTO.TransactionDTO;
-
-import java.util.List;
-import java.util.ArrayList;
+import Database.DatabaseConnection;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TransactionDAO {
 
@@ -18,15 +23,27 @@ public class TransactionDAO {
     }
 
     public static List<TransactionDTO> listTransaction() {
-        return transactionList;
+        MongoDatabase db = DatabaseConnection.mongoClient.getDatabase("HW2Database");
+        MongoCollection<Document> transactionList = db.getCollection("Transactions");
     }
 
     public static boolean isTransactionValid(TransactionDAO Instance) {
+        // check if payment method exists
         return true;
     }
 
-
     public void createTransaction(String paymentMethod, String itemCode) {
-        transactionList.add(new TransactionDTO(paymentMethod,itemCode));
+        MongoDatabase db = DatabaseConnection.mongoClient.getDatabase("HW2Database");
+        MongoCollection<Document> transactionList = db.getCollection("Transactions");
+
+        // Create new DTO and convert to JSON
+        TransactionDTO transaction = new TransactionDTO(paymentMethod, itemCode);
+        String transactionJSON = gson.toJson(transaction);
+
+        // Create new mongo Document from JSON
+        Document newTransaction = Document.parse(transactionJSON);
+
+        // Add Document to Collection
+        transactionList.insertOne(newTransaction);
     }
 }
